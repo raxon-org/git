f3b9d587899c2338aea254e94e8b7f18bc368266
Remco van der Velde <remco@universeorange.com>
2025-09-23 15:55:24 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -19,25 +19,17 @@ trait Main {
     {
         Core::interactive();
         $object = $this->object();
-
-        $dir = new Dir();
-        $read = $dir->read('/mnt/Vps3/Mount/Domain/');
-        foreach($read as $nr => $file){
-            if($file->type === Dir::TYPE){
-                $test_dir = $file->url . '.git' . $object->config('ds');
-                if(Dir::exist($test_dir)){
-                    //add $file->url to 
-                    $command = 'app raxon/git repository add -directory="' . $file->url . '"';
-                    Core::execute($object, $command, $output, $notification);
-                    if($output){
-                        echo $output . PHP_EOL;
-                    }
-                    if($notification){
-                        echo $notification . PHP_EOL;
-                    }
-                }
-            }
-        }        
+        $node = new Node($object);
+        $class = 'System.Git';
+        $role = $node->role_system();
+        $response = $node->list(
+            $class,
+            $role,
+            [
+                'limit' => 100000                
+            ]
+        );
+        ddd($response);
     }
 }
 
