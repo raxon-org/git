c3629cbaa5420d8268b53fe6630c993659aeaf57
Remco van der Velde <remco@universeorange.com>
2025-09-23 15:32:43 +0200

    init

--- src/Package/Trait/Main.php
+++ src/Package/Trait/Main.php
@@ -22,7 +22,22 @@ trait Main {
 
         $dir = new Dir();
         $read = $dir->read('/mnt/Vps3/Mount/Domain/');
-        ddd($read);
+        foreach($read as $nr => $file){
+            if($file->type === Dir::TYPE){
+                $test_dir = $file->url . '.git' . $object->config('ds');
+                if(Dir::exist($test_dir)){
+                    //add $file->url to 
+                    $command = 'app raxon/git repository add -directory="' . $test_dir . '"';
+                    Core::execute($object, $command, $output, $notification);
+                    if($output){
+                        echo $output . PHP_EOL;
+                    }
+                    if($notification){
+                        echo $notification . PHP_EOL;
+                    }
+                }
+            }
+        }        
     }
 }
 
